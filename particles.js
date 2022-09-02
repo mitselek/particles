5755ca8ad279ba1b0e86c55074d1432f06595937
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2022-09-02 19:32:54 +0300

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,37 +1,9 @@
-// Rules values
-    //   const factor = 1
-    //   const RULES = {
-    //     green: {
-    //       green: 0.878214014158254 * factor,
-    //       red: 0.383942932294564 * factor,
-    //       yellow: 0.3632328353781209 * factor,
-    //       blue: 0.4357079645785089 * factor,
-    //     },
-    //     red: {
-    //       green: -0.8131279812066854 * factor,
-    //       red: 0.8761564046567396 * factor,
-    //       yellow: -0.686246916739194 * factor,
-    //       blue: -0.42403398294928163 * factor,
-    //     },
-    //     yellow: {
-    //       green: 0.8283611643992606 * factor,
-    //       red: -0.8050409003234531 * factor,
-    //       yellow: 0.8422661062679588 * factor,
-    //       blue: -0.6206303204367405 * factor,
-    //     },
-    //     blue: {
-    //       green: -0.6276679142294777 * factor,
-    //       red: -0.48726835984229977 * factor,
-    //       yellow: -0.8155039608681607 * factor,
-    //       blue: 0.49503848830455155 * factor,
-    //     },
-    //   }
 const MASSLESS_FRACTION = 1/3 
 const MAX_MASS = 5
 const G = 10
 const G_FACTOR = 1/2
 const E = -5
-const E_FACTOR = -1/2
+const E_FACTOR = 1.01
 const C = 10 // speed limit
 const SPEED_LIMIT = C
 
