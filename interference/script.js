cc5ef901e3e44b6631345526db56c1ba3f05aab7
mitselek <mihkel.putrinsh@gmail.com>
2022-09-09 11:58:32 +0300

    handover

--- interference/script.js
+++ interference/script.js
@@ -1,7 +1,7 @@
 const C = 299792458 // speed limit in m/s
-const P2M = 0.05 // how many meters per point
-const P_SIZE = 1
-const NUM_ANTENNAS = 2
+const P2M = 1 // how many meters per point
+const P_SIZE = 5
+const NUM_ANTENNAS = 3
 const FREQUENCY = 142800000
 const WAVELENGTH = C / FREQUENCY
 const WAVELENGTH_PT = WAVELENGTH / P2M
@@ -23,10 +23,10 @@ updateCanvasDimensions()
 const context = canvas.getContext("2d")
 
 
-const TARGET = {x:800, y:400}
+const TARGET = {x:1200, y:100, r:20}
 const antenna_array = {
-  x: 300,
-  y: 300,
+  x: 400,
+  y: 500,
   r: WAVELENGTH_PT * 0.5,
   antennas: []
 }
@@ -51,7 +51,7 @@ for (let i = 0; i < NUM_ANTENNAS; i++) {
     antenna( FREQUENCY, 
              x, 
              y, 
-             shift = (Math.hypot(TARGET.x-x, TARGET.y-y) % WAVELENGTH_PT) / WAVELENGTH_PT + 1/4, 
+             shift = 0, // (Math.hypot(TARGET.x-x, TARGET.y-y) % WAVELENGTH_PT) / WAVELENGTH_PT + 1/4, 
              amplitude = 255,
              { R:Math.random()*255,
                G:Math.random()*255,
@@ -72,44 +72,65 @@ const movaverage = (key, value) => {
 
 // Update Frames
 let prev_ms = new Date().valueOf() - 1000/60
-update(true)
+update(1)
 
-function update(first_frame) {
+function update(antenna_to_tune) {
 
-  const draw_target = () => {
-    context.strokeStyle = "white"
+  const draw_target = (x=TARGET.x, y=TARGET.y, r=TARGET.r, c="white") => {
+    context.strokeStyle = c
     // context.fillRect(x, y, sx, sy)
     context.beginPath()
-    context.arc(TARGET.x, TARGET.y, 10, 0, 2 * Math.PI, false)
+    context.arc(x, y, r, 0, 2 * Math.PI, false)
     context.stroke()
   }
 
   const draw = (x, y, intensity) => {
-    // context.fillStyle = "hsl(120, 100%, " + intensity + "%)"
-    // console.log('fs', context.fillStyle)
     context.fillStyle = "rgb("+255*Math.abs(intensity.R)+","+255*Math.abs(intensity.G)+","+255*Math.abs(intensity.B)+")"
+    // console.log('fs', context.fillStyle)
     context.fillRect( x, y, P_SIZE, P_SIZE )
   }
 
+  // I'm looking for highest intensity at immediate surroundings of TARGET
   const tune = (the_antenna) => {
-    const distance_steps = 100
+    // console.log('tuning', JSON.stringify(the_antenna))
+    the_antenna.shift = 0 // reset the antenna
+    const target_distance_steps = TARGET.r
+    const target_angle_steps = 10
     const phase_steps = 360
     const best_tuning = {phase: 0, value: 0}
-    for (let p = 0; p < 360; p++) {
-      const phase = 1 / phase_steps * p
-      for (let i = 0; i < distance_steps; i++) {
-        const target = {
-          x: (TARGET.x - antenna_array.x) / distance_steps * i,
-          y: (TARGET.y - antenna_array.y) / distance_steps * i,
-        }
-        for (let aix = 0; aix < antenna_array.antennas.length; aix++) {
-          const antenna = antenna_array.antennas[aix]
-          const distance_pt = Math.hypot(target.x - antenna.x, target.y - antenna.y)
-          const intensity = getIntensity0(distance_pt, antenna)
-          
-        }
+    for (let p = 0; p < phase_steps; p++) {
+      the_antenna.shift = p / phase_steps
+      let sum_intensity = sumIntensity(target_distance_steps, target_angle_steps)
+      // console.log('sum of intensity', sum_intensity)
+      sum_intensity = sum_intensity / target_distance_steps / target_angle_steps
+      if (sum_intensity > best_tuning.value) {
+        best_tuning.phase = the_antenna.shift
+        best_tuning.value = sum_intensity
+        // console.log('Found better tuning', JSON.stringify(best_tuning))
       }
     }
+    the_antenna.shift = best_tuning.phase
+    console.log('post tuning', JSON.stringify(best_tuning))
+  }
+
+  const tune2 = () => {
+    const target_angle_steps = 10
+    const current_tuning = antenna_array.antennas.map(a => a.shift)
+    let best_tuning = {phase_conf: antenna_array.antennas.map(a => a.shift), value: sumIntensity(TARGET.r, target_angle_steps)}
+    console.log('previous best', JSON.stringify(best_tuning))
+    for (let i = 0; i < 1000; i++) {
+      for (let aix = 0; aix < antenna_array.antennas.length; aix++) {
+        antenna_array.antennas[aix].shift = Math.random()
+      }
+      const sum_intensity = sumIntensity(TARGET.r, target_angle_steps)
+      if (sum_intensity > best_tuning.value) {
+        best_tuning = {phase_conf: antenna_array.antennas.map(a => a.shift), value: sum_intensity}
+      }
+    }
+    for (let i = 0; i < best_tuning.phase_conf.length; i++) {
+      antenna_array.antennas[i].shift = best_tuning.phase_conf[i];
+    }
+    console.log('new best', JSON.stringify(best_tuning))
   }
 
   // Update Canvas Dimensions - if screen size changed
@@ -119,38 +140,61 @@ function update(first_frame) {
   document.getElementById('FPS').innerText = Math.round(movaverage('fps', 1000 / (now_ms - prev_ms)))
   prev_ms = now_ms
 
+  // tune(antenna_array.antennas[antenna_to_tune])
+  tune2()
+
   // m.clearRect(0, 0, canvas.width, canvas.height)
   // draw(0, 0, "black", canvas.width/3, canvas.height/2)
   for (let cx = 0; cx < canvas.width; cx += P_SIZE) {
     for (let cy = 0; cy < canvas.height; cy += P_SIZE) {
       // let field = 0
       let field = {R:0, G:0, B:0}
-      if (first_frame === false) {
-        // draw(cx, cy, field)
-        // continue
-      }
-      for (let a = 0; a < antenna_array.antennas.length; a++) {
-        const antenna = antenna_array.antennas[a]
-        const distance_pt = Math.hypot(antenna.x-cx, antenna.y-cy)
-        // const distance_pt = Math.sqrt((antenna.x-cx)**2 + (antenna.y-cy)**2)
-        // const distance_pt = Math.abs(antenna.x-cx) + Math.abs(antenna.y-cy)
-        // const distance_m = distance_pt * P2M
-        const intensity = getIntensity0(distance_pt, antenna)
-        field.R += intensity * antenna.color.R
-        field.G += intensity * antenna.color.G
-        field.B += intensity * antenna.color.B
-        // field += intensity
-      }
-      // const intensity = Math.round(Math.abs(field * 100))
+      // for (let a = 0; a < antenna_array.antennas.length; a++) {
+      //   const antenna = antenna_array.antennas[a]
+      //   const distance_pt = Math.hypot(antenna.x-cx, antenna.y-cy)
+      //   // const distance_pt = Math.sqrt((antenna.x-cx)**2 + (antenna.y-cy)**2)
+      //   // const distance_pt = Math.abs(antenna.x-cx) + Math.abs(antenna.y-cy)
+      //   // const distance_m = distance_pt * P2M
+      //   const intensity = getIntensity0(distance_pt, antenna)
+      //   field.R += intensity * antenna.color.R
+      //   field.G += intensity * antenna.color.G
+      //   field.B += intensity * antenna.color.B
+      //   // field += intensity
+      // }
+      // // const intensity = Math.round(Math.abs(field * 100))
       // console.log(field)
-      draw(cx, cy, field)
+      // draw(cx, cy, field)
+      const intensity = getIntensity(cx, cy) * 255
+      field.R += intensity
+      field.G += intensity
+      field.B += intensity
+    draw(cx, cy, field)
     }
   }
   draw_target()
+  for (let a = 0; a < antenna_array.antennas.length; a++) {
+    const antenna = antenna_array.antennas[a]
+    draw_target(antenna.x, antenna.y, 5, "black")
+  }
   console.log(antenna_array.antennas.map(a => a.shift))
   // antenna_array.antennas[0].shift += Math.random()/2000
   // antennas[1].shift += 0.15
-  requestAnimationFrame(() => update(false))
+  requestAnimationFrame(() => update((antenna_to_tune + 1) % antenna_array.antennas.length))
+
+  function sumIntensity(target_distance_steps, target_angle_steps) {
+    let sum_intensity = 0
+    for (let r = 0; r < target_distance_steps; r++) { // scan around the TARGET
+      for (let i = 0; i < target_angle_steps; i++) {
+        const theta = 2 * Math.PI / target_angle_steps * i
+        const poi = {
+          x: TARGET.x + Math.sin(theta) * r,
+          y: TARGET.y + Math.cos(theta) * r
+        }
+        sum_intensity += getIntensity(poi.x, poi.y)
+      }
+    }
+    return sum_intensity
+  }
 }
 
 function getIntensity(x, y) {
@@ -162,7 +206,7 @@ function getIntensity(x, y) {
     let p_intensity = 0
     for (let aix = 0; aix < antenna_array.antennas.length; aix++) {
       const antenna = antenna_array.antennas[aix]
-      const phase_reminder_pt = (distance_pt + (antenna.shift + phase_incr) * antenna.wavelength_pt) % antenna.wavelength_pt
+      const phase_reminder_pt = (distance_pt[aix] + (antenna.shift + phase_incr) * antenna.wavelength_pt) % antenna.wavelength_pt
       const sin_t = Math.sin(phase_reminder_pt / antenna.wavelength_pt * 2 * Math.PI)
       p_intensity += antenna.amplitude * sin_t / distance_pt[aix] ** 2
     }
